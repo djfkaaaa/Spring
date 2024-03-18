89e044debadb88c55d63f83621e6486c02934dcf
“djfkaaaa” <wkdghva@gmail.com>
2024-03-18 18:05:14 +0900

    spring

---
+++ src/main/java/com/james/api/HomeController.java
@@ -0,0 +1,12 @@
+package com.james.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @RequestMapping("/")
+    public String hello(){
+        return "Welcome to Spring Boot!";
+    }
+}
